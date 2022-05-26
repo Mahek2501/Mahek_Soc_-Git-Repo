e91e63faaf8509f65d854ad1d842e558266dc6e4
Mahek2501 <101885811+Mahek2501@users.noreply.github.com>
2022-05-26 23:59:15 +0530

    Add files via upload

---
+++ js 1.js
@@ -0,0 +1,27 @@
+ let name = 'mahek';
+ console.log(name);
+ //cannot be a reserved keyword
+ // should be meaningful
+ // cannot start with a number
+ //  cannot contain space or hyphen(-)
+ // they are case-sensitive
+ // to use two words in one variable right them like
+ // firstName ( 1st letter of 1st word smaal and 1st  letter of 2nd word capital.
+
+
+ //let  firstName = "mahek", lastName = 'maheswari';
+ // or
+ let  firstName = "mahek"; //better practice
+let lastName = 'maheshwari';
+
+
+/*const interestRate = 0.3; //if you put const there then you cannot redeclare its value as it will give error
+interestRate = 1;         // if you want to redeclare put let
+console.log(interestRate)*/
+
+let name2 = 'mahek';
+let age = 18;
+let isApproved = false;
+let firstName2 = undefined;
+let selectedColor = null;
+
---
+++ js 2.js
@@ -0,0 +1,16 @@
+//objects
+let person = {
+    name : 'mahek',
+    age : 18
+};
+console.log(person);
+
+//dot notation
+person. name = "suhani";
+
+// bracket notation
+//person ['name'] = "papa" ;
+// or
+// let selection = 'name';
+//person[selection] = 'papa';
+console.log(person.name) ; 
